5f6b5a485ca11b65fae8d6adac3acda740eb6fcb
jeff kang <jeffyjkang@gmail.com>
2022-08-17 20:30:47 -0500

    week_3 lecture

--- array_methods.js
+++ array_methods.js
@@ -1,8 +1,63 @@
 // map -> returns new array new array
+const numArr = [1, 2, 3, 4]
+// using map, multiply each of the elements in numArr by 2 -> [2, 4, 6, 8]
+function multiplyByTwo(arr) {
+  const multipliedArr = arr.map(function(element) {
+    return element * 2
+  })
+  return multipliedArr
+}
+
+console.log(multiplyByTwo(numArr))
+
+const subArr = [3, 6, 9, 12]
+// using map, subtract 2 from each of the elements in the array, -> [1, 4, 7, 10]
+
+function subTwoFrom(arr){
+  return arr.map(function(element){
+      return element - 2
+});
+}
+
+console.log(subTwoFrom(subArr))
+
 
 // reduce -> call back function, optional initial value, within the callback function 2 args, previous value and current value
+// add all elements in the array together
+function addAllElements(arr) {
+
+  // arr looks like this: [1, 2, 3, 4]
+  const reducedArr = arr.reduce(function(acc, cur) {
+
+    return acc += cur
+  }, 0)
+  return reducedArr
+}
+
+console.log(addAllElements(numArr))
+
+// return everything multiplied, 1* 2* 3 * 4 = 24
+
+function multiplyAllElements(arr){
+
+  const reducedArr = arr.reduce(function(acc, cur) {
+      return acc *= cur
+  }, 1)
+  return reducedArr
+  }
+  console.log(multiplyAllElements(numArr));
+
+
 
 // filter -> returns shallow copy, function that passes a 'test'
+
+function onlyEvenNumbers(arr) {
+  return arr.filter(function(el) {
+    return el % 2 === 0
+  })
+}
+
+console.log(onlyEvenNumbers(numArr))
 
 // for each -> doesn't return, calls function for each element in array
 // let fruits = ['apple', 'orange', 'banana']
--- init_arrays.js
+++ init_arrays.js
@@ -1,39 +1,33 @@
 // array
 
 // 1.
-const nums = [1,2,52,4,5,28,7]
+const numsArr = [1,2,52,4,5,28,7]
+// [0,1,2,3,4,5,6] index values
+// [1,2,3,4,5,6,7] end result
 // set nums element value in order from 1 - 7
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function setNums(arr) {
+  arr[2] = 3
+  arr[5] = 6
+  return arr
+}
+console.log(numsArr)
+console.log(setNums(numsArr))
 
 // 2.
 const cars = ['buick', 'chevy', 'ford', 'honda', 'mini', 'toyota']
 // while keeping alphabetical order add 'kia', 'jeep', 'bmw'
+// cars = ['bmw','buick','chevy','ford','honda','jeep','kia','mini','toyota']
+
+// cars = ['bmw','buick','chevy','ford','honda','jeep','kia','mini','toyota']
+function carOrder(arr) {
+  arr.push('kia', 'jeep', 'bmw');
+  arr.sort();
+  console.log(arr);
+}
+
+carOrder(cars);
+
 
 // 3.
 const board = [
@@ -42,7 +36,32 @@ const board = [
   ['', 'O', 'X']
 ]
 // finish the board so that x wins
+board[2][0] = 'X'
+console.log(board)
 
 // 4.
 const newBoard = []
 // using a loop create a 2d array newBoard that looks like the board above, don't worry about populating the board
+
+// 1 loop, from index 0 to index 2, newBoard[i] = ['','','']
+// 2 loops, outer loop index 0 to index 2, in inner loop index 0 to 2 , if its even index i%2 ===0 'x' else 'o'
+
+function createTicTacToeBoard(emptyBoard) {
+  for (let i=0; i<3; i++) {
+    emptyBoard[i] = ['x','o','x']
+  }
+  console.log(emptyBoard)
+
+  // for (let i =0; i<3; i++) {
+  //   for(let j=0; j<3; j++) {
+  //     emptyBoard[i][j] = 'x'
+  //     if j %2 === 0 {
+  //       emptyBoard[i][j] = 'x'
+  //     } else {
+  //       emptyBoard[i][j] = 'o'
+  //     }
+  //   }
+  // }
+}
+
+createTicTacToeBoard(newBoard)
